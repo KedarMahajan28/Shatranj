b5eb199ba599f1a55f7d6ef86ad3b7543d0fdf62
KedarMahajan28 <mahajankedarp@gmail.com>
2026-03-31 01:23:33 +0530

    z

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify('http://localhost:8000')
-  },
   server: {
     port: 3000,
     proxy: {
