16592431a3f80ff4d277261abf531da7025e5396
KedarMahajan28 <mahajankedarp@gmail.com>
2026-04-02 22:29:16 +0530

    socket :/

--- Frontend/src/utils/socket.js
+++ Frontend/src/utils/socket.js
@@ -2,14 +2,24 @@ import { io } from 'socket.io-client';
 
 let socket = null;
 
-
 export function getSocket() {
   if (!socket) {
-    socket = io('/', {            
+    const socketUrl = import.meta.env.VITE_API_URL || window.location.origin;
+    
+    socket = io(socketUrl, {            
       withCredentials: true,
       transports: ['websocket', 'polling'],
       reconnectionAttempts: 5,
       reconnectionDelay: 1500,
+      reconnectionDelayMax: 5000,
+    });
+
+    socket.on('connect_error', (error) => {
+      console.error('[Socket] Connection error:', error.message);
+    });
+
+    socket.on('disconnect', (reason) => {
+      console.log('[Socket] Disconnected:', reason);
     });
   }
   return socket;
